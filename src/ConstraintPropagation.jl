e69a3d1f3b8fc6fc20e911f925811fa270f69af4
David Sanders <dpsanders@gmail.com>
2016-04-15 22:13:04 -0400

    Package base file ConstraintPropagation.jl

--- src/ConstraintPropagation.jl
+++ src/ConstraintPropagation.jl
@@ -1,5 +1,10 @@
 module ConstraintPropagation
 
-# package code goes here
+export Domain, add_constraint, apply_contractor, initialize, @contractor
+
+include("contractor.jl")
+include("reverse_mode.jl")
+include("domain.jl")
+
 
 end # module
