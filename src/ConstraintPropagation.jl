e40fc0090302c5a1eb482997ddfb60fea275e2ac
David Sanders <dpsanders@gmail.com>
2016-05-03 14:04:39 -0400

    @add_constraint -> @constraint

--- src/ConstraintPropagation.jl
+++ src/ConstraintPropagation.jl
@@ -7,7 +7,7 @@ using MacroTools
 
 
 export
-    Domain, add_constraint, apply_contractor, initialize, @contractor, apply_all_contractors, @add_constraint
+    Domain, add_constraint, apply_contractor, initialize, @contractor, apply_all_contractors, @constraint
 
 
 include("reverse_mode.jl")
