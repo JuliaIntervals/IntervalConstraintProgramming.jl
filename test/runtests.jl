9ea8fb4bd5accb655b3352a4a960d457f19d50ef
David Sanders <dpsanders@gmail.com>
2016-06-04 14:11:29 -0400

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 
-using ConstraintPropagation
+using ConstraintProgramming
 using ValidatedNumerics
 
 using FactCheck
@@ -13,16 +13,22 @@ facts("Separator tests") do
     @fact inner --> (-1..1, -1..1)
     @fact boundary --> (II, II)
 
+    II = -∞..∞
+    X = IntervalBox(II, II)
+    inner, boundary = S(X)
+    @fact inner --> (-1..1, -1..1)
+    @fact boundary --> (II, II)
 
-    S1a = @constraint 1x > 0
-    S1b = @constraint 1y > 0
+
+    S1a = @constraint x > 0
+    S1b = @constraint y > 0
 
     S1 = S1a ∩ S1b
-    inner, boundary = set_inversion(S7, IntervalBox(-3..3, -3..3), 0.1)
+    inner, boundary = set_inversion(S1, IntervalBox(-3..3, -3..3), 0.1)
     @fact inner --> [IntervalBox(1.5..3, 0..3), IntervalBox(0..1.5, 0..3)]
 
     S2 = S1a ∪ S1b
-    inner, boundary = set_inversion(S7, IntervalBox(-3..3, -3..3), 0.1)
+    inner, boundary = set_inversion(S2, IntervalBox(-3..3, -3..3), 0.1)
     @fact inner --> [IntervalBox(-3..0, 0..3), IntervalBox(0..3, -3..3)]
-    
+
 end
