c6fd4e1d84d8d0b862288be6b30c45dfac634a40
David Sanders <dpsanders@gmail.com>
2016-06-04 14:10:27 -0400

    Add intersection and union tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,8 +9,20 @@ facts("Separator tests") do
     X = IntervalBox(II, II)
     S = @constraint x^2 + y^2 <= 1
 
-    inner, outer = S(X)
+    inner, boundary = S(X)
     @fact inner --> (-1..1, -1..1)
-    @fact outer --> (II, II)
+    @fact boundary --> (II, II)
 
+
+    S1a = @constraint 1x > 0
+    S1b = @constraint 1y > 0
+
+    S1 = S1a ∩ S1b
+    inner, boundary = set_inversion(S7, IntervalBox(-3..3, -3..3), 0.1)
+    @fact inner --> [IntervalBox(1.5..3, 0..3), IntervalBox(0..1.5, 0..3)]
+
+    S2 = S1a ∪ S1b
+    inner, boundary = set_inversion(S7, IntervalBox(-3..3, -3..3), 0.1)
+    @fact inner --> [IntervalBox(-3..0, 0..3), IntervalBox(0..3, -3..3)]
+    
 end
