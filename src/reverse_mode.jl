0e171bf77674b497049c0abc21459188372d19e6
David P. Sanders <dpsanders@gmail.com>
2016-06-08 16:03:53 -0400

    Add sqrtRev function and solid torus example (#14)
    * Small refactoring of rev_ops and add sqrtrev
    
    * Add solid torus notebook

--- src/reverse_mode.jl
+++ src/reverse_mode.jl
@@ -1,9 +1,18 @@
+const rev_ops = Dict(
+                    :+     => :plusRev,
+                    :*     => :mulRev,
+                    :^     => :powerRev,
+                    :-     => :minusRev,
+                    :sqrt  => :sqrtRev
+                    )
+
+
 function plusRev(a::Interval, b::Interval, c::Interval)  # a = b + c
     a = a ∩ (b + c)
     b = b ∩ (a - c)
     c = c ∩ (a - b)
 
-    a, b, c
+    return a, b, c
 end
 
 plusRev(a,b,c) = plusRev(promote(a,b,c)...)
@@ -13,7 +22,7 @@ function minusRev(a::Interval, b::Interval, c::Interval)  # a = b - c
     b = b ∩ (a + c)
     c = c ∩ (b - a)
 
-    a, b, c
+    return a, b, c
 end
 
 minusRev(a,b,c) = minusRev(promote(a,b,c)...)
@@ -24,11 +33,13 @@ function mulRev(a::Interval, b::Interval, c::Interval)  # a = b * c
     b = b ∩ (a / c)
     c = c ∩ (a / b)
 
-    a, b, c
+    return a, b, c
 end
 
 mulRev(a,b,c) = mulRev(promote(a,b,c)...)
 
+
+Base.iseven(x::Interval) = isinteger(x) && iseven(x.lo)
 
 function powerRev(a::Interval, b::Interval, c::Interval)  # a = b^c,  log(a) = c.log(b),  b = a^(1/c)
 
@@ -39,14 +50,35 @@ function powerRev(a::Interval, b::Interval, c::Interval)  # a = b^c,  log(a) = c
         b2 = b ∩ (-√a)
 
         b = hull(b1, b2)
+
+    elseif iseven(c)
+        b1 = b ∩ ( a^(inv(c) ))
+        b2 = b ∩ ( -( a^(inv(c)) ) )
+
+        b = hull(b1, b2)
+
     else
+
         b = b ∩ ( a^(inv(c) ))
     end
 
     a = a ∩ (b ^ c)
     c = c ∩ (log(a) / log(b))
 
-    a, b, c
+    return a, b, c
 end
 
 powerRev(a,b,c) = powerRev(promote(a,b,c)...)
+
+
+function sqrtRev(a::Interval, b::Interval)  # a = sqrt(b)
+    a1 = a ∩ √b
+    a2 = a ∩ (-(√b))
+    a = hull(a1, a2)
+
+    b = b ∩ (a^2)
+
+    return a, b
+end
+
+sqrtRev(a,b) = sqrtRev(promote(a,b)...)
