6af0ed129d8ad7a1dd5a4c8d6abae2c20c9d5e5a
David Sanders <dpsanders@gmail.com>
2016-12-26 18:01:02 +0000

    Try different documenter settings

--- docs/make.jl
+++ docs/make.jl
@@ -5,12 +5,22 @@ makedocs(
     modules = Module[IntervalConstraintProgramming],
     doctest = true,
     format = :html,
+    authors = "David P. Sanders"
     sitename = "IntervalConstraintProgramming.jl"
     )
 
+# deploydocs(
+#     deps = Deps.pip("pygments", "mkdocs", "mkdocs-cinder", "python-markdown-math"),
+#     repo   = "github.com/dpsanders/IntervalConstraintProgramming.jl.git",
+#     julia = "0.5",
+#     osname = "linux"
+# )
+
 deploydocs(
-    deps = Deps.pip("pygments", "mkdocs", "mkdocs-cinder", "python-markdown-math"),
-    repo   = "github.com/dpsanders/IntervalConstraintProgramming.jl.git",
+    repo = "github.com/dpsanders/IntervalConstraintProgramming.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing,
     julia = "0.5",
     osname = "linux"
 )
