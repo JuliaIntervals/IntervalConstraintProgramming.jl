611ef0b4dc5897d6681bb4ce41d3e33f847ce07c
Eeshan Gupta <eeshan9815@gmail.com>
2018-07-29 15:42:37 +0530

    Add non-exposed function for contracting gradients

--- src/IntervalConstraintProgramming.jl
+++ src/IntervalConstraintProgramming.jl
@@ -27,6 +27,7 @@ const reverse_operations = IntervalContractors.reverse_operations
 const ARRAY_TYPES = (:AbstractArray, :AbstractVector, :AbstractMatrix, :Array, :Vector, :Matrix)
 const REAL_TYPES = (:Bool, :Integer, :(Irrational{:e}), :(Irrational{:π}), :Rational, :BigFloat, :BigInt, :AbstractFloat, :Real)
 const FUNCTIONS = ((:Base, :+, 2), (:Base, :/, 2), (:Base, :^, 2), (:Base, :asin, 1), (:Base, :cos, 1), (:Base, :exp, 1), (:Base, :*, 2), (:Base, :abs, 1), (:Base, :log, 1), (:Base, :-, 2), (:Base, :sqrt, 1), (:Base, :tan, 1), (:Base, :sin, 1), )
+# ^(x::Interval{Float64}, p::Integer) = pow(x, p)
 
 include("ast.jl")
 include("code_generation.jl")
